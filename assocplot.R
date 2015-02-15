6835fd0adf3c1e3ac20c6230a9827a2b388f982c
nasion78 <sungwook78@gmail.com>
2015-02-16 00:13:11 +0900

    주석 추가

--- assocplot.R
+++ assocplot.R
@@ -1,3 +1,4 @@
+# Association Plot 
 require(graphics)
 M <- as.table(cbind(c(18,28,14), c(20,51,28), c(12,25,9)))
 dimnames(M) <- list(Husband = c("Tall", "Medium", "Short"), Wife = c("Tall", "Medium", "Short"))
