3e8ed8ea53e63c521530fa21bff8ac9c494bee0c
nasion78 <sungwook78@gmail.com>
2015-02-16 12:57:10 +0900

    Mosaic Plot code Add

--- spineplot.R
+++ spineplot.R
@@ -1,4 +1,3 @@
-# Spine Plot
 require(graphics)
 r1 <- c(7.9, 67.6, 28.3, 53.6)
 r2 <- c(4.4, 54.5, 29.9, 57.6)
@@ -8,5 +7,12 @@ r5 <- c(8.5, 46.3, 32.2, 50)
 data <- as.table(rbind(r1, r2, r3, r4, r5))
 dimnames(data) <- list(Town = c("Sheffield", "Warrington", "Dewsbusy", "Leicester", "Gloucester"), 
                                 Vaccinated = c("Yes(<10)", "No(<10)", "Yes(>10)", "No(>10)"))
+
+# Spine Plot
 spineplot(data)
 spineplot(data, col=c("darkred", "orange", "darkgreen", "yellow1")) #spine graph color option
+
+# Mosaic Plot
+mosaicplot(data, color=1:4)
+mosaicplot(data, color=2:5)  #color pattern change
+mosaicplot(data, color=1:4, las=1) # text orientation horizontal
